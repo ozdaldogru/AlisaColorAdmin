e611f7ecdc8edfa43b6e7b3aa26e0fce6e320f7a
Ozdal Dogru <ozdaldogru@gmail.com>
2025-02-08 00:45:48 -0500

    Add 'Collections' column to product table

--- components/products/ProductColumns.tsx
+++ components/products/ProductColumns.tsx
@@ -22,6 +22,10 @@ export const columns: ColumnDef<ProductType>[] = [
     header: "Status",
   },
   {
+    accessorKey: "collections",
+    header: "Collections",
+  },
+  {
     accessorKey: "price",
     header: "Price ($)",
   },
